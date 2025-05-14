aaa88e486ec32aabd6689e5b2a34012b0d194fc6
Emily Bennet <me@emilybennet.com>
2025-05-14 17:09:35 -0400

    Use exact versions in script.

--- scripts/versions.js
+++ scripts/versions.js
@@ -3,31 +3,47 @@ const execSync = require("child_process").execSync;
 
 const data = require("../package.json");
 
-const getCurrentVersions = async () => {
+const getExactVersions = () => {
+  const latest = execSync(`npm ls --json`).toString();
+  const parsed = JSON.parse(latest);
+
+  return Object.keys(parsed.dependencies).reduce((acc, cur) => {
+    acc[cur] = parsed.dependencies[cur].version;
+    return acc;
+  }, {});
+};
+
+const getCurrentVersions = () => {
   const now = new Date();
   const stamp = Math.floor(now.getTime() / 1000);
+  const exactVersion = getExactVersions();
   const output = [];
 
+  console.log(
+    `🚀 Generating a breakdown of installed packages, the installed version, and the latest on NPM. Please stand by~\n`
+  );
   output.push("# Installed package versions vs. latest");
   output.push(`File version ${stamp}`);
 
   output.push("\n## Dependencies");
 
   Object.keys(data.dependencies).forEach((key) => {
-    const latest = execSync(`npm view ${key} version`)
-      .toString()
-      .replace("\n", "");
-    output.push(`${key}, ${data.dependencies[key]}, ${latest}`);
+    const latest = execSync(`npm view ${key} version`).toString().trim();
+    // output.push(`${key}, ${data.dependencies[key]}, ${latest}`);
+    output.push(`${key}, ${exactVersion[key]}, ${latest}`);
   });
 
   output.push("\n## Dev Dependencies");
 
   Object.keys(data.devDependencies).forEach((key) => {
     const latest = execSync(`npm view ${key} version`).toString();
-    output.push(`${key}, ${data.devDependencies[key]}, ${latest}`);
+    // output.push(`${key}, ${data.devDependencies[key]}, ${latest}`);
+    output.push(`${key}, ${exactVersion[key]}, ${latest}`);
   });
 
-  fs.writeFileSync(`${__dirname}/version-${stamp}.txt`, output.join("\n"));
+  const file = `${__dirname}/version-${stamp}.txt`;
+  fs.writeFileSync(file, output.join("\n"));
+  console.log(`🏁 Version breakdown successfully written to:\n${file}`);
 };
 
 getCurrentVersions();
